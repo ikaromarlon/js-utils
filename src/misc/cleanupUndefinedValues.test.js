c14ec502575e15c540de093579e538d7f06f6c3f
Icaro Silva <ikaromarlon@gmail.com>
2023-11-30 07:38:14 +0100

    test(misc): add cleanupUndefinedValues

---
+++ src/misc/cleanupUndefinedValues.test.js
@@ -0,0 +1,11 @@
+import { describe, test } from 'node:test'
+import { strict as assert } from 'node:assert'
+import cleanupUndefinedValues from './cleanupUndefinedValues.js'
+
+describe('cleanupUndefinedValues', () => {
+  test.todo('should recursively remove properties with undefined values from an object or array', () => {
+    const data = { a: 1, b: undefined, c: { d: 2, e: undefined }, f: [undefined, 3] }
+
+    assert.deepEqual(cleanupUndefinedValues(data), { a: 1, c: { d: 2 }, f: [3] })
+  })
+})
