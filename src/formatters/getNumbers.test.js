b98edd8846f1e556bda42dc0230d6e95d9084b7f
Icaro Silva <ikaromarlon@gmail.com>
2023-11-30 11:44:56 +0100

    test(formatters): add getNumbers

---
+++ src/formatters/getNumbers.test.js
@@ -0,0 +1,10 @@
+import { describe, test } from 'node:test'
+import { strict as assert } from 'node:assert'
+import getNumbers from './getNumbers.js'
+
+describe('getNumbers', () => {
+  test.todo('should extract numeric digits from a string', () => {
+    assert.equal(getNumbers('abc123xyz'), '123')
+    assert.equal(getNumbers(45.67), '4567')
+  })
+})
