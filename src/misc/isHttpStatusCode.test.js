1c2506eab6be3089b8141a6141a13d987aae116d
Icaro Silva <ikaromarlon@gmail.com>
2023-11-30 08:23:34 +0100

    test(misc): add isHttpStatusCode

---
+++ src/misc/isHttpStatusCode.test.js
@@ -0,0 +1,22 @@
+import { describe, test } from 'node:test'
+import { strict as assert } from 'node:assert'
+import isHttpStatusCode from './isHttpStatusCode.js'
+
+describe('isHttpStatusCode', () => {
+  test.todo('should return true', () => {
+    assert.equal(isHttpStatusCode(100), true)
+    assert.equal(isHttpStatusCode(200), true)
+    assert.equal(isHttpStatusCode(201), true)
+    assert.equal(isHttpStatusCode(300), true)
+    assert.equal(isHttpStatusCode(302), true)
+    assert.equal(isHttpStatusCode(400), true)
+    assert.equal(isHttpStatusCode(404), true)
+    assert.equal(isHttpStatusCode(500), true)
+    assert.equal(isHttpStatusCode(502), true)
+  })
+
+  test.todo('should return false', () => {
+    assert.equal(isHttpStatusCode(700), false)
+    assert.equal(isHttpStatusCode('abc'), false)
+  })
+})
