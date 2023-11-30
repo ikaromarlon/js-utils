cc26397147f1bf5d5c7a9066ea8567d8f1ae5ef5
Icaro Silva <ikaromarlon@gmail.com>
2023-11-30 08:29:04 +0100

    test(formatters): add removeAccents

---
+++ src/formatters/removeAccents.test.js
@@ -0,0 +1,10 @@
+import { describe, test } from 'node:test'
+import { strict as assert } from 'node:assert'
+import removeAccents from './removeAccents.js'
+
+describe('removeAccents', () => {
+  test.todo('should removes accents (diacritics) from a string', () => {
+    assert.equal(removeAccents('cliché'), 'cliche')
+    assert.equal(removeAccents('über'), 'uber')
+  })
+})
