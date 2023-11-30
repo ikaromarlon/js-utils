0246a1d45d8c53e06bf2f68338341187913195fd
Icaro Silva <ikaromarlon@gmail.com>
2023-11-30 08:10:53 +0100

    feat(misc): add sleep

---
+++ src/misc/sleep.js
@@ -0,0 +1,19 @@
+import secondsToMilliseconds from '../date-time/secondsToMilliseconds.js'
+
+/**
+ * Sleeps (delays) for the specified number of seconds.
+ *
+ * @param {number} seconds - The number of seconds to sleep.
+ * @returns {Promise<void>} A promise that resolves after the specified sleep duration.
+ * @example
+ * async function exampleUsage() {
+ *   console.log('Start');
+ *   await sleep(2);
+ *   console.log('After 2 seconds');
+ * }
+ * // Call the exampleUsage function
+ * exampleUsage();
+ */
+export default function sleep (seconds) {
+  return new Promise(resolve => setTimeout(resolve, secondsToMilliseconds(seconds)))
+}
