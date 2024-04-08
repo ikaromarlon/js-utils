db4713be08519d9e88e30d9dbca7d19a81bdfe3b
Icaro Silva <ikaromarlon@gmail.com>
2024-04-08 12:08:39 +0200

    refactor: change sleep parameter from seconds to milliseconds and use default value 0

--- src/misc/sleep.js
+++ src/misc/sleep.js
@@ -1,20 +1,18 @@
-import secondsToMilliseconds from '../date-time/secondsToMilliseconds.js'
-
 /**
  * Sleeps (delays) for the specified number of seconds.
  *
- * @param {number} seconds - The number of seconds to sleep.
+ * @param {number} [time=0] - The amount of time to sleep in milliseconds. Defaults to 0 if not provided.
  * @returns {Promise<void>} A promise that resolves after the specified sleep duration.
  *
  * @example
  * async function exampleUsage() {
  *   console.log('Start')
- *   await sleep(2)
+ *   await sleep(2000)
  *   console.log('After 2 seconds')
  * }
  * // Call the exampleUsage function
  * await exampleUsage()
  */
-export default function sleep (seconds) {
-  return new Promise(resolve => setTimeout(resolve, secondsToMilliseconds(seconds)))
+export default function sleep (time = 0) {
+  return new Promise(resolve => setTimeout(resolve, time))
 }
--- src/misc/sleep.test.js
+++ src/misc/sleep.test.js
@@ -1,15 +1,14 @@
 import { describe, test } from 'node:test'
 import { strict as assert } from 'node:assert'
 import sleep from './sleep.js'
-import millisecondsToSeconds from '../date-time/millisecondsToSeconds.js'
 
 describe('sleep', () => {
   test('should sleep (delay) for the specified number of seconds', async () => {
     const start = new Date()
-    await sleep(1)
+    await sleep(1000)
     const end = new Date()
     const timeDiffMs = end - start
-    const timeDiffSec = millisecondsToSeconds(timeDiffMs)
+    const timeDiffSec = timeDiffMs / 1000
     assert.equal(Math.round(timeDiffSec), 1)
   })
 })
